7b7ff8c66a37d327610983d6c1c29f3104daf664
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-06-01 17:06:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ require('dotenv').config();
 const { Telegraf, Markup } = require('telegraf');
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-// Import Plugins
+// 1. Saare Modules Import karo
 const shop = require('./shop');
 const deposit = require('./deposit');
 const account = require('./account');
 const admin = require('./admin');
+const feedback = require('./feedback'); // Agar hai toh
 
-// Initialize Plugins
+// 2. Har module ko bot ka access de do
 shop.setup(bot);
 deposit.setup(bot);
 account.setup(bot);
 admin.setup(bot);
+feedback.setup(bot);
 
+// 3. Main Menu (Jo tere screenshot jaisa dikhega)
 bot.start((ctx) => {
-  ctx.reply('🛒 MENU:', Markup.inlineKeyboard([
+  ctx.reply('🛒 *GAMING KEY SHOP*\n\n🔑 Premium Gaming Keys Marketplace...', {
+    parse_mode: 'Markdown',
+    ...Markup.inlineKeyboard([
       [Markup.button.callback('• Purchase Key •', 'shop_menu')],
       [Markup.button.callback('• Account •', 'acc_menu'), Markup.button.callback('• History •', 'hist_menu')],
       [Markup.button.callback('• Deposit Fund (₹) •', 'dep_inr'), Markup.button.callback('• Deposit Fund ($) •', 'dep_usd')],
       [Markup.button.callback('• Feedback •', 'feed_menu')]
-  ]));
+    ])
+  });
 });
 
 bot.launch();
