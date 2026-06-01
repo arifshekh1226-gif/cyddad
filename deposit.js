bc23a340f5c8008da57f339444a8642d717b8351
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-06-01 16:32:29 +0530

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,16 +1,17 @@
-// deposit.js
 const { Markup } = require('telegraf');
 
 function setupDeposit(bot) {
-  // Jab button dabaaye
+  // Button click handle
   bot.action('deposit', (ctx) => {
-    ctx.replyWithPhoto('https://i.postimg.cc/63XQc9wg/IMG-20260601-162334-289.jpg', {
-      caption: '💰 Is QR par payment karein aur screenshot yahan bhejein.'
+    ctx.answerCbQuery();
+    ctx.replyWithPhoto('https://postimg.cc/QBfsxZFJ', {
+      caption: '💰 Payment karein aur screenshot yahan bhejein.'
     });
   });
 
-  // Jab screenshot aaye
+  // Photo receive handle
   bot.on('photo', async (ctx) => {
+    // Check agar admin ko bhej rahe hain (Sirf deposit ke liye)
     await ctx.telegram.sendPhoto(process.env.ADMIN_ID, ctx.message.photo[0].file_id, {
       caption: `⚠️ New Deposit from ${ctx.from.first_name}\nID: ${ctx.from.id}`
     });
