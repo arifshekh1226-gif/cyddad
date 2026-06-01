c7ebcfcf2af40de47510adfa825b7def4b0248f6
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-06-01 14:37:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const { Telegraf, Markup } = require('telegraf');
+require('dotenv').config();
 
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+// /start command - Menu dikhane ke liye
+bot.start((ctx) => {
+  ctx.reply('Welcome to Key Shopping Bot! 🛒', Markup.inlineKeyboard([
+    [Markup.button.callback('Purchase Key', 'purchase_key')],
+    [Markup.button.callback('Account', 'account'), Markup.button.callback('History', 'history')],
+    [Markup.button.callback('Deposit Fund', 'deposit')]
+  ]));
+});
+
+// Button ka action (Jab koi click kare)
+bot.action('purchase_key', (ctx) => {
+  ctx.reply('Select your game:', Markup.inlineKeyboard([
+    [Markup.button.callback('KOS CARROM', 'carrom')],
+    [Markup.button.callback('KOS 8BP', '8bp')]
+  ]));
+});
+
+bot.launch();
+console.log("Bot is running with Buttons...");
