c72eec80463bd75c022ec1cc529425309f4cf0be
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-06-01 17:12:02 +0530

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -1,1 +1,11 @@
-
+// feedback.js
+module.exports = {
+  setup: (bot) => {
+    bot.action('feed_menu', (ctx) => {
+      ctx.editMessageText('📝 *Feedback Panel*\n\nApna feedback yahan likhein:', {
+        parse_mode: 'Markdown'
+      });
+      // Logic: Feedback collect karke admin ko bhej do
+    });
+  }
+};
