8667fb39a9464bc0d84686d5edce5b55c5528cb0
arifshekh1226-gif <arifshekh1226@gmail.com>
2026-06-01 17:19:03 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,9 +1,8 @@
-// shop.js
+// shop.js file ka content
 module.exports = {
   setup: (bot) => {
-    // Yahan tera saara logic aayega
     bot.action('shop_menu', (ctx) => {
-      ctx.reply('Shop menu active!');
+      ctx.reply('Shop menu is working!');
     });
   }
 };
